5c9974c3f5646e344aa454f54bd8af77db4a0943
Debayan <dgupta@altiostar.com>
2020-04-26 19:40:48 +0530

    Added Query Parameter

--- src/main/java/com/altiostar/demo/HelloApp.java
+++ src/main/java/com/altiostar/demo/HelloApp.java
@@ -3,15 +3,17 @@ package com.altiostar.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class HelloApp {
-
-  @RequestMapping("/")
-  public String home() {
-    return "Hello Docker World";
+@RequestMapping(value = "/hello", 
+			  method = RequestMethod.GET)
+  public String sayHello(@RequestParam("name") String name) {
+    return name + ", say Hello Kubernetes World";
   }
 
   public static void main(String[] args) {
@@ -19,7 +21,3 @@ public class HelloApp {
   }
 
 }
-
-
-
-

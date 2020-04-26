f6c109748c383dd90e5124e7e9c0c3158acdf7f9
Debayan <dgupta@altiostar.com>
2020-04-26 21:02:22 +0530

    Fixed surefire version to run junits

--- src/test/java/com/altiostar/demo/HelloAppTest.java
+++ src/test/java/com/altiostar/demo/HelloAppTest.java
@@ -1,14 +1,14 @@
 package com.altiostar.demo;
 
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
-class HelloAppTest {
+
+public class HelloAppTest {
 
 	@Test
-	void sayHelloPass() {
+	public void sayHelloPass() {
 		HelloApp app = new HelloApp();
 		String result = app.sayHello("Dev");
 		assertTrue(result.equalsIgnoreCase("Dev, say Hello Kubernetes World"));
